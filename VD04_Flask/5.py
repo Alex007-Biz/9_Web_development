31851da3146ea6c8cf7ea039cfdff4a292f2e1ce
Alex007-Biz <bizukin@yandex.ru>
2024-06-07 09:19:44 +0300

    vd04-5

---
+++ VD04_Flask/5.py
@@ -0,0 +1,14 @@
+import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def films():
+    return render_template("films.html")
+
+@app.route("/person/")
+def person():
+    return render_template("heroes.html")
+
+if __name__ == "__main__":
+    app.run()
