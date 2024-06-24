533539b26ac2c8fcf6207d49c27bc34db65b765e
Alex007-Biz <bizukin@yandex.ru>
2024-06-24 22:07:03 +0300

    good

--- VD09_развертывание на хостинге/app/routes.py
+++ VD09_развертывание на хостинге/app/routes.py
@@ -22,7 +22,7 @@ def register():
         db.session.commit()
         flash('Вы успешно зарегистрировались!', category='success')
         return redirect(url_for('login'))
-    return render_template("register.html")
+    return render_template("register.html", form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,7 +36,7 @@ def login():
             return redirect(url_for('index'))
         else:
             flash('неверно введены данные аккаунта', category='danger')
-    return render_template("login.html")
+    return render_template("login.html", form=form)
 
 @app.route('/logout')
 def logout():
