67b9fbb3ed3238033093c48e05a0e967aeed55bc
Alex007-Biz <bizukin@yandex.ru>
2024-06-25 22:20:53 +0300

    hw

--- DJ01/WebDGProject/ZeroCoder/main/views.py
+++ DJ01/WebDGProject/ZeroCoder/main/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django!!</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это вторая страница на Django!!</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Это страница Data на Django!!</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это страница Test на Django!</h1>")
