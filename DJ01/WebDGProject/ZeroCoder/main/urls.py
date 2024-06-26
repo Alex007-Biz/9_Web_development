5daaaa9ae4c1aa2a8017e3a1394ee4067253d403
Alex007-Biz <bizukin@yandex.ru>
2024-06-26 22:00:28 +0300

    2

--- DJ01/WebDGProject/ZeroCoder/main/urls.py
+++ DJ01/WebDGProject/ZeroCoder/main/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new', views.new),
-    path('data', views.data),
-    path('test', views.test),
 ]
--- DJ01/WebDGProject/ZeroCoder/main/views.py
+++ DJ01/WebDGProject/ZeroCoder/main/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django!!</h1>")
+    return render(request, template_name: 'main/index.html')
 
-def new(request):
-    return HttpResponse("<h1>Это вторая страница на Django!!</h1>")
-
-def data(request):
-    return HttpResponse("<h1>Это страница Data на Django!!</h1>")
-
-def test(request):
-    return HttpResponse("<h1>Это страница Test на Django!</h1>")
+# def new(request):
+#     return HttpResponse("<h1>Это вторая страница на Django!!</h1>")
+#
+# def data(request):
+#     return HttpResponse("<h1>Это страница Data на Django!!</h1>")
+#
+# def test(request):
+#     return HttpResponse("<h1>Это страница Test на Django!</h1>")
