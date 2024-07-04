f698c327a585171c26eaace89c4ccf45030d2d08
Alex007-Biz <bizukin@yandex.ru>
2024-07-04 08:35:07 +0300

    8

--- DJ01/WebDGProject/ZeroCoder/news/views.py
+++ DJ01/WebDGProject/ZeroCoder/news/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import News_post
 # Create your views here.
+
+
 def home(request):
     news = News_post.objects.all()
     return render(request, 'news/news.html', {'news': news})
+
+
+def create_news(request):
+    return render(request, 'news/add_new_post.html')
